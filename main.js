a37be36ef0cec2c87164c71f628115792b55b72f
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-07-23 20:44:21 +0000

    Caso uso #2
    feat: :construction: Caso de Uso 2 en Progreso
    
    feat: construction: Caso de Uso 2 pendiente actualizar documentos
    
    feat: :recycle: refactorizacion para mejorar logica de jugadores
    
    feat: :sparkles: Caso de Uso 2 realizado

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-import { casoUso4 } from "./src/modules/resultados/controller/resultados.controller.js"
+import {JugadoresRepository} from "./src/modules/jugadores/repository/jugadores.repository.js"
+import {casoUso1} from "./src/modules/equipos/controller/equipos.controller.js"
+import {casoUso2} from "./src/modules/jugadores/controller/jugadores.controller.js"
+import {casoUso8} from "./src/modules/arbitros/controller/arbitros.controller.js"
+//Prueba
+/*
+const jugadores = new JugadoresRepository();
+console.log(await jugadores.getAllJugadores());
+*/
 
-casoUso4()
+//casoUso1()
+
+/**
+ * 
+ * @param accion Se puede pasar "editar", "eliminar", "agregar"
+ * 
+ * * Si usa "agregar", por favor, pasar la informacion en jugadores.controller.js 
+ *   para agregar el documento
+ * 
+ * * Si usa "eliminar", por favor, pasar el _id del jugador a eliminar en jugadores.controller.js
+ * 
+ * * Si usa "editar", por favor, especificar el campo y el nuevo valor en los parametros de la funcion GestionJugador
+ *  de jugadores.service.js
+ * 
+ *  
+ */
+
+casoUso2("eliminar")
+//casoUso8()
--- src/modules/jugadores/repository/jugadores.repository.js
+++ src/modules/jugadores/repository/jugadores.repository.js
@@ -17,6 +17,20 @@ export class JugadoresRepository extends Connect {
         let res = await this.collection.find({},{projection: {nombre:1,_id:0}}).toArray();
         return res;
     }
+    async aggregateJugador(object) {
+        let res = await this.collection.insertOne(object)
+        return res
+    }
+
+    async deleteJugador(filter) {
+        let res = await this.collection.deleteOne(filter);
+        return res;
+    }
+    
+    async updateJugador(filter, update) {
+        let res = await this.collection.updateOne(filter, { $set: update });
+        return res;
+    }
 
     async getJugadorById(id){
         let objectId = new ObjectId(id)
