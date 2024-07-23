fff074ff45307261b21f219d796ce82b60669716
SneatX <santi02005@hotmai.com>
2024-07-22 22:08:26 -0500

    feat: :sparkles: Validacion fecha disponible en el estadio

--- src/modules/partidos/controller/partidos.controller.js
+++ src/modules/partidos/controller/partidos.controller.js
@@ -2,7 +2,7 @@ import { registroPartido } from "../service/partidos.service.js";
 
 export async function casoUso4() {
     let equipoVisitante = "669ae0e3d2847f9a6eac5fff";
-    let fecha = "2024-07-20T15:30:00.000Z";
+    let fecha = "2024-06-01T18:30:00Z";
     let estadio = "669eb05aee5df68095c5cca5";
     
     let res = await registroPartido(equipoVisitante, fecha, estadio)
--- src/modules/partidos/service/partidos.service.js
+++ src/modules/partidos/service/partidos.service.js
@@ -1,7 +1,7 @@
 import { EquiposRepository } from "../../equipos/repository/equipos.repository.js";
 import { EstadiosRepository } from "../../estadios/repository/estadios.repository.js";
-
-export async function registroPartido(idEquipoVisita, fecha, idEstadio){
+import { ObjectId } from "mongodb";
+export async function registroPartido(idEquipoVisita, fecha, idEstadio) {
 
     //Validar existencia del equipo visitante 
     let equiposCollection = new EquiposRepository()
@@ -10,16 +10,36 @@ export async function registroPartido(idEquipoVisita, fecha, idEstadio){
         equiposCollection.getEquipoById(idEquipoVisita),
         estadiosCollection.getEstadioById(idEstadio)
     ])
-    if(!equipoVisitante) return {resultado: "error", mensaje: "Equipo visita inexistente"}
+    if (!equipoVisitante) return { resultado: "error", mensaje: "Equipo visita inexistente" }
 
-    if(!estadio) return {resultado: "error", mensaje: "Estadio inexistente"}
+    if (!estadio) return { resultado: "error", mensaje: "Estadio inexistente" }
 
     //Buscar equipo local
     let equipoLocal = await equiposCollection.getEquipoByEstadioId(idEstadio)
 
-    if(!equipoLocal) return {resultado: "error", mensaje: "Equipo local inexistente"}
+    if (!equipoLocal) return { resultado: "error", mensaje: "Equipo local inexistente" }
 
-    if(equipoLocal._id.toString() === equipoVisitante._id.toString()){
-        return {resultado: "error", mensaje: "El equipo local y visitante son iguales"}
+    if (equipoLocal._id.toString() === equipoVisitante._id.toString()) {
+        return { resultado: "error", mensaje: "El equipo local y visitante son iguales" }
+    }
+
+    //Validacion disponibilidad del estadio en la fecha
+
+    for (let horario of estadio.Horarios) {
+        let fechaInicioRegistrada = new Date(horario);
+        let fechaFinRegistrada = new Date(horario);
+        fechaFinRegistrada.setHours(fechaFinRegistrada.getHours() + 4);
+
+        let fechaInicioDeseada = new Date(fecha);
+        let fechaFinDeseada = new Date(fecha);
+        fechaFinDeseada.setHours(fechaFinDeseada.getHours() + 4);
+
+        // Verificar si los intervalos se cruzan
+        if (
+            (fechaInicioDeseada >= fechaInicioRegistrada && fechaInicioDeseada <= fechaFinRegistrada) || //Fecha de inicio esta entre los intervalos
+            (fechaFinDeseada >= fechaInicioRegistrada && fechaFinDeseada <= fechaFinRegistrada) //Fecha de fin esta entre los intervalos
+        ) {
+            return { resultado: "error", mensaje: "El intervalo de tiempo deseado cruza con un horario registrado" };
+        }
     }
 }
