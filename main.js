ad1471b2e3f75f608acaeba958ef5b6ab03d35d1
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-07-24 15:15:39 +0000

    feat: :sparkles: Caso de Uso 2 realizado

--- main.js
+++ main.js
@@ -11,9 +11,19 @@ console.log(await jugadores.getAllJugadores());
 //casoUso1()
 
 /**
+ * 
  * @param accion Se puede pasar "editar", "eliminar", "agregar"
- *  Si usa "agregar", por favor, pasar al archivo jugadores.controller.js 
- *  para agregar el documentos
+ * 
+ * * Si usa "agregar", por favor, pasar la informacion en jugadores.controller.js 
+ *   para agregar el documento
+ * 
+ * * Si usa "eliminar", por favor, pasar el _id del jugador a eliminar en jugadores.controller.js
+ * 
+ * * Si usa "editar", por favor, especificar el campo y el nuevo valor en los parametros de la funcion GestionJugador
+ *  de jugadores.service.js
+ * 
+ *  
  */
+
 casoUso2("eliminar")
 //casoUso8()
